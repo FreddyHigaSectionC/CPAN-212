b5065a03e0873079a9c590dbb9646fe3e1650417
Freddy Higa <f_higa77@hotmail.com>
2025-04-05 15:07:25 -0400

    Create BudgetItem, BudgetList and CreateBudget

--- labs/lab05/src/app/(routes)/dashboard/page.jsx
+++ labs/lab05/src/app/(routes)/dashboard/page.jsx
@@ -8,6 +8,7 @@ import { Budgets, Expenses, Incomes } from '@/utils/schema';
 import BarChartDashboard from './_components/BarChartDashboard';
 import BudgetItem from './budgets/_components/BudgetItem';
 import ExpenseListTable from './expenses/_components/ExpenseListTable';
+
 function Dashboard() {
   const { user } = useUser();
 
